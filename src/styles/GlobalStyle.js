1021bcca003516b06128570e07c68ecab4fa99b0
이선호 <dltjsgho94@gmail.com>
2022-01-17 18:36:51 +0900

    Add: 초기세팅 완료

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+	*, html { 
+    box-sizing: border-box;
+   
+   a {text-decoration: none;
+    color:black;
+} 
+
+}
+
+@media screen and (min-width: 360px) and (max-width: 759px) {
+  body{
+    width: 100%;
+    height: 100%;
+  }
+}
+
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  grey: 'rgba(196,196,196,0.3)',
+  middleGrey: 'rgba(65,65,65,0.4)',
+  deepGrey: '#828282',
+  background: '#fafafa',
+  white: '#FFFFFF',
+  box: '#e3effd',
+  fontColor: '#000000',
+  main: '#3478f6',
+};
+
+export default theme;
